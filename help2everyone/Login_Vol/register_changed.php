b54c0a6e9d0f6949bf5048cc6a7af4827d96e869
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-05-18 11:27:26 +0100

    Small changes

--- help2everyone/Login_Vol/register_changed.php
+++ help2everyone/Login_Vol/register_changed.php
@@ -4,6 +4,7 @@ include('./../classes/Mail.php');
 
 $ermsg=0;
 session_start();
+
 if ($_SERVER['REQUEST_METHOD']=='GET') {
 
 	if (!empty($_GET['Nome'])) {
@@ -24,58 +25,161 @@ if ($_SERVER['REQUEST_METHOD']=='GET') {
 }
 
 if ($_SERVER['REQUEST_METHOD']=='POST') {
+	
+	$data = [];
+	if (!empty($_POST['Nome'])) {
+		$array["nome"] = $InserirNome = htmlspecialchars(stripslashes($_POST['Nome']));
+	}
+	if (!empty($_POST['Apelido'])) {
+		$array["apelido"] = $Inserirapelido = htmlspecialchars(stripslashes($_POST['Apelido']));
+	}
+	if (!empty($_POST['Email'])) {
+		$array["email"] = $Inseriremail = htmlspecialchars(stripslashes($_POST['Email']));
+	}
+	if (!empty($_POST['User'])) {
+		$array["user"] = $Inseriruser = htmlspecialchars(stripslashes($_POST['User']));
+	}
+	if (!empty($_POST['Pass'])) {
+		$array["pass"] = hash('sha256', htmlspecialchars(stripslashes($_POST['Pass'])));
+		//$pass=htmlspecialchars($_POST['Pass']);
+		//$pass=hash('sha256', $pass);
+	}
+	if (!empty($_POST['ConfirmarPass'])) {
+		$conpass = hash('sha256', htmlspecialchars(stripslashes($_POST['ConfirmarPass'])));
+		//$conpass=htmlspecialchars($_POST['ConfirmarPass']);
+		//$conpass=hash('sha256', $conpass);
+	}
 
+	/* Erros
+	1 - Utilizador já registado
+	2 - Email já registado
+	3 - Password fraca
+	4 - As passwords não coincidem
+	*/
 
-    if (!empty($_POST['Nome'])) {
-        $nome = $InserirNome = htmlspecialchars($_POST['Nome']);
-    }
-    if (!empty($_POST['Apelido'])) {
-        $apelido = $Inserirapelido = htmlspecialchars($_POST['Apelido']);
-    }
-    if (!empty($_POST['Email'])) {
-        $email = $Inseriremail =htmlspecialchars($_POST['Email']);
-    }
-    if (!empty($_POST['User'])) {
-        $user = $Inseriruser = htmlspecialchars($_POST['User']);
-    }
-    if (!empty($_POST['Pass'])) {
-        $pass = htmlspecialchars(hash('sha256', $_POST['Pass']));
-    }
-    if (!empty($_POST['ConfirmarPass'])) {
-        $conpass = htmlspecialchars(hash('sha256', $_POST['ConfirmarPass']));
-    }
-    $foto='userimg.png';
-    $userinc=mysqli_query($conn,"SELECT Utilizador FROM tblvoluntario WHERE Utilizador='".$user."'");
-    $linha=mysqli_fetch_array($userinc);
+	$searchUser = select_tblvoluntario(["Utilizador"], ["Utilizador"], $array["user"]);
+	$searchEmail = select_tblvoluntario(["Email"], ["Email"], $array["email"]);
 
-    $emailinc=mysqli_query($conn,"SELECT Email FROM tblvoluntario WHERE Email='".$email."'");
-    $coluna=mysqli_fetch_array($emailinc);
+	if($array["pass"] != $conpass)
+		$ermsg = 4;
+	else if($array["pass"] == hash('sha256', '1234')
+		|| $array["pass"] == hash('sha256', '4321')
+		|| $array["pass"] == hash('sha256', '123456')
+		|| $array["pass"] == hash('sha256', '654321')
+		|| $array["pass"] == hash('sha256', $array['user'])
+		|| $array["pass"] == hash('sha256', $array['nome'])
+		|| $array["pass"] == hash('sha256', $array['apelido'])
+		|| $array["pass"] == hash('sha256', $array['email']))
+		$ermsg = 3;
+	else if($searchEmail->num_rows > 0)
+		$ermsg = 2;
+	else if($searchUser->num_rows > 0)
+		$ermsg = 1;
+		
+	$array['foto'] = 'userimg.png';
+	$array['data'] = date('y-m-d');
 
-    $pass1=hash('sha256', '1234');
-    $pass2=hash('sha256', '4321');
-    $pass3=hash('sha256', '123456');
-    $pass4=hash('sha256', '654321');
-    $pass5=hash('sha256', $user);
-    $pass6=hash('sha256', $nome);
-    $pass7=hash('sha256', $apelido);
-    $pass8=hash('sha256', $email);
-    $data= date('y-m-d');
+	/*
+	$userinc=mysqli_query($conn,"SELECT Utilizador FROM tblvoluntario WHERE Utilizador='".$user."'");
+	$linha=mysqli_fetch_array($userinc);
 
-    if($linha[0]!=null){
-        $ermsg=1;
+	$emailinc=mysqli_query($conn,"SELECT Email FROM tblvoluntario WHERE Email='".$email."'");
+	$coluna=mysqli_fetch_array($emailinc);*/
+	
+	if($ermsg == 0){
+		//if(insert_tblvoluntario($array)){
+		$ar = 1;
+		if($ar == 0){
+			/* Mail::sendMail('Bem-vindo ao Help2Everyone!',"A sua Conta como Voluntário foi criada, a partir de agora vai poder ajudar mais os outros do que pensa.</br></br><p>(Caso não reconheça este endereço de Email, ou se não o tiver
+      		desejado, simplesmente ignore-o ou apague-o.)</p>",$email); */ 
+			$_SESSION['user']=$user;
+			//header('Location: ./../index.php');
+		}else{
+			$ermsg = 5;
+		}
+	}
+	//header("Location: ".$_SERVER['PHP_SELF']."?msg=".$ermsg);
+    /*if($linha[0]!=null){
+      $ermsg=1;
     }else if($coluna[0]!=null){
-        $ermsg=2;
-        }else if($pass==$pass1 or $pass==$pass2 or $pass==$pass3 or $pass==$pass4 or $pass==$pass5 or $pass==$pass6 or $pass==$pass7 or $pass==$pass8 ){
-            $ermsg=3;
-        }else if($pass!=$conpass){
-        $ermsg=4;
-    }else{
-        mysqli_query($conn,"insert into tblvoluntario(Nome,Apelido,Email,Utilizador,Password,Registo,Foto) values('".$nome."','".$apelido."','".$email."','".$user."','".$pass."','".$data."','".$foto."')");
-        Mail::sendMail('Bem-vindo ao Help2Everyone!',"A sua Conta como Voluntário foi criada, a partir de agora vai poder ajudar mais os outros do que pensa.</br></br><p>(Caso não reconheça este endereço de Email, ou se não o tiver
-        desejado, simplesmente ignore-o ou apague-o.)</p>",$email);
-        $_SESSION['user']=$user;
-        header('Location: ./../index.php');
-    }
+      $ermsg=2;
+	}*/
+
+	/*mysqli_query($conn,"insert into tblvoluntario(Nome,Apelido,Email,Utilizador,Password,Registo,Foto) values('".$nome."','".$apelido."','".$email."','".$user."','".$pass."','".$data."','".$foto."')");
+	Mail::sendMail('Bem-vindo ao Help2Everyone!',"A sua Conta como Voluntário foi criada, a partir de agora vai poder ajudar mais os outros do que pensa.</br></br><p>(Caso não reconheça este endereço de Email, ou se não o tiver
+	desejado, simplesmente ignore-o ou apague-o.)</p>",$email);
+	$_SESSION['user']=$user;
+	//header('Location: ./../index.php');*/
+}
+
+function insert_tblvoluntario($array) : boolval{
+	// Using mysqli to prevent agains sql injection
+	$success = True;
+	
+	// Prepared statement, stage 1: prepare
+	if (!($query = $mysqli->prepare("INSERT INTO tblvoluntario(Nome, Apelido, Email, Utilizador, Password, Registo, Foto) VALUES (?,?,?,?,?,?,?)"))) {
+		//echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		$success = 	False;
+   	}
+
+	// Prepared statement, stage 2: bind and execute 
+	if (!$success || !$query->bind_param('sssssss',$array['nome'], $array['apelido'], $array['email'], 
+	$array['user'], $array['pass'], $array['data'], $array['foto'])) {
+		//echo "Binding parameters failed: (" . $query->errno . ") " . $query->error;
+		$success = False;
+	}
+	
+	if (!$success || !$query->execute()) {
+		//echo "Execute failed: (" . $query->errno . ") " . $query->error;
+		$success = False;
+	}
+
+	// Close the statement 
+	$query->close();
+
+	return $success;
+}
+
+function select_tblvoluntario($select_fields, $cond_fields, $array_data){
+	$success = True;
+	// Build the condition fields
+	for($i = 0; $i < count($cond_fields); $i++){
+		$questions .= $cond_fields[$i].' = ?';
+		if(($i+1) < count($cond_fields))
+			$questions.= ', ';
+	}
+	// Build the select fields
+	for($i = 0; $i < count($select_fields); $i++){
+		$conditions .= $select_fields[$i];
+		if(($i+1) < count($cond_fields))
+			$conditions.= ', ';
+	}
+	$types = str_repeat('s', count($array_data));
+	$sql = "SELECT {$conditions} FROM tblvoluntario WHERE {$questions} ";
+
+/*	// Prepared statement, stage 1: prepare
+	if (!($query = $mysqli->prepare($sql))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		$success = 	False;
+	}
+	/*
+	// Prepared statement, stage 2: bind and execute 
+	if (!$success || !$query->bind_param($types, $array_data)) {
+		//echo "Binding parameters failed: (" . $query->errno . ") " . $query->error;
+		$success = False;
+	}
+	
+	if (!$success || !$query->execute()) {
+		//echo "Execute failed: (" . $query->errno . ") " . $query->error;
+		$success = False;
+	}
+
+	$res = $query->get_result();
+
+	// Close the statement 
+	$query->close();
+	*/
+	return $res;
 }
 
 ?>
@@ -110,19 +214,19 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 </head>
 <body style="background-color: #666666;">
 	<?php
-	if($ermsg==1){
+	if($ermsg == 1){
 		echo "<div class='alert alert-warning' role='alert'>
 					Utilizador já registado!!!
 					</div>";
-	}else if($ermsg==2){
+	}else if($ermsg == 2){
 		echo "<div class='alert alert-warning' role='alert'>
 					Email já registado!!!
 					</div>";
-	}else if($ermsg==3){
+	}else if($ermsg == 3){
 		echo "<div class='alert alert-warning' role='alert'>
 					Palavra-Passe fraca!!!
 					</div>";
-	}else if($ermsg==4){
+	}else if($ermsg == 4){
 		echo "<div class='alert alert-warning' role='alert'>
 					As Palavras-Passe não coincidem, por favor tente novamente!!!
 					</div>";
